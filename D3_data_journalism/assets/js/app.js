1434df91c4c6d93006ae60c76d95e9fada171d79
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-16 21:29:28 -0700

    changed where script and link tags are in index.html, added error handling to app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,8 @@
   // Get the data
-  d3.csv("./assets/data/data.csv").then((data) => {
+  d3.csv("./assets/data/data.csv", d3.autoType).then((data) => {
     console.log(data);// @TODO: YOUR CODE HERE!
  
-  });
+  }).catch(error => console.log(error));
+
 
 
