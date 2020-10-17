cef6fc489dee74fa32ac2d455831b65de96135c9
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-16 22:19:15 -0700

    added SVG container

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,4 +1,31 @@
-  // Get the data
+ // Set up the SVG area
+ // Store the dimensions of the SVG container
+var svgWidth = 1140;
+var svgHeight = 600;
+
+// Create an object to represent the chart's margins within the SVG container
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Create and size the SVG container. Then append, size, and position an SVG group inside within margins defined earlier
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+ 
+ 
+ 
+ // Get the data
   d3.csv("./assets/data/data.csv", d3.autoType).then((data) => {
     console.log(data);// @TODO: YOUR CODE HERE!
  
